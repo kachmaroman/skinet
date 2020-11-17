4762988062add84cf5580def3dafb13e495cf637
Roman Kachmar <lnt397@gmail.com>
2020-11-18 00:34:38 +0200

    Initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ProductsController : ControllerBase
+	{
+		private readonly StoreContext _storeContext;
+
+		public ProductsController(StoreContext storeContext)
+		{
+			_storeContext = storeContext;
+		}
+
+		[HttpGet]
+		public async Task<List<Product>> GetProductsAsync()
+		{
+			return await _storeContext.Products.ToListAsync();
+		}
+
+		[HttpGet]
+		[Route("{id}")]
+		public async Task<Product> GetProductAsync([FromRoute] int id)
+		{
+			return await _storeContext.Products.FindAsync(id);
+		}
+	}
+}
