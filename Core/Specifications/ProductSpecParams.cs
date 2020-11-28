89f98e3db0ffd3e1d4ace5d6170bb0cd8b77c964
Roman Kachmar <lnt397@gmail.com>
2020-11-28 17:08:45 +0200

    Added search functionality

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -5,6 +5,7 @@
 		private const int MaxPageSize = 50;
 
 		private int _pageSize = 6;
+		private string _search;
 
 		public int PageSize
 		{
@@ -13,8 +14,17 @@
 		}
 
 		public int PageIndex { get; set; } = 1;
+
 		public int? BrandId { get; set; }
+
 		public int? TypeId { get; set; }
+
 		public string Sort { get; set; }
+
+		public string Search
+		{
+			get => _search;
+			set => _search = value?.ToLower();
+		}
 	}
 }
--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -6,7 +6,10 @@ namespace Core.Specifications
 	public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
 	{
 		public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams) 
-			: base(x => (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
+			: base(x => 
+				(string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+				(!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+				(!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
 		{
 			Include(x => x.ProductBrand);
 			Include(x => x.ProductType);
